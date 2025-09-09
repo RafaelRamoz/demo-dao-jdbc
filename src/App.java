533eba8fee25a90fc12ccaa77f189712a6a9d61d
RafaelRamoz <rafa.ramos2014@hotmail.com>
2025-09-08 23:24:02 -0300

    Insert Implementation

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -8,17 +10,19 @@ import model.entities.Seller;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
         System.out.println("\n=== TEST 01 = Find By ID ===");
         
-        SellerDao sellerdao = DaoFactory.createSellerDao();
-        Seller seller = sellerdao.findById(3);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
 
         System.out.println("\n\n=== TEST 02 = Find by Department ===");
 
         Department department = new Department(null, 2);
-        List<Seller> list = sellerdao.findByDepartment(department);
+        List<Seller> list = sellerDao.findByDepartment(department);
         for(Seller obj: list){
             System.out.println(obj);
         }
@@ -27,10 +31,17 @@ public class App {
 
         System.out.println("\n\n=== TEST 03 = Find All ===");
 
-        List<Seller> allList = sellerdao.findAll();
+        List<Seller> allList = sellerDao.findAll();
         for(Seller obj: allList){
             System.out.println(obj);
         }
+
+
+        System.out.println("\n\n=== TEST 04 = Seller Insert ===");
+
+        Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", LocalDate.parse("01/01/2000", fmt), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
 
     }
 }
